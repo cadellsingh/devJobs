760f4ddbbf21bbf099737f5b1cb27903a05ab9f3
Cadell Singh <cadellsingh9@gmail.com>
2020-12-14 18:37:13 -0400

    styled form

--- src/Form.js
+++ src/Form.js
@@ -2,22 +2,37 @@ const Form = () => {
   const handleOnSubmit = () => {};
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <label htmlFor="">
+    <form id="form-container" onSubmit={handleOnSubmit}>
+      <div className="title">
+        <label htmlFor="title" />
         <input
+          aria-labelledby="title"
           type="text"
+          name="title"
+          id="title"
           placeholder="Filter by title, companies, expertise"
         />
-      </label>
+      </div>
 
-      <label htmlFor="">
-        <input type="text" placeholder="Filter by location" />
-      </label>
+      <div className="location">
+        <label htmlFor="location" />
 
-      <label htmlFor="">
-        <input type="checkbox" />
-        Full Time Only
-      </label>
+        <input
+          aria-labelledby="location"
+          type="text"
+          name="location"
+          id="location"
+          placeholder="Filter by location"
+        />
+      </div>
+
+      <div className="search-container">
+        <div className="checkbox">
+          <input type="checkbox" name="fullTime" id="fullTime" />
+          <label htmlFor="fullTime">Full Time Only </label>
+        </div>
+        <button type="submit">Search</button>
+      </div>
     </form>
   );
 };
