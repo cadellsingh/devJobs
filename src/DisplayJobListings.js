0599d6a5dfb192bc26790a1193a18356c5faa5ec
Cadell Singh <cadellsingh9@gmail.com>
2020-12-14 14:24:57 -0400

    Create DisplayJobListings.js

---
+++ src/DisplayJobListings.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import JobCard from "./JobCard";
+import JobList from "./JobList";
+
+const DisplayJobListings = ({ jobData }) => {
+  const [displayStyle, setDisplayStyle] = useState("grid");
+
+  // can have a component here for grid or list icons
+
+  const jobDetails = jobData === undefined ? null : jobData[0];
+
+  return (
+    <div>
+      {/*this will be grid or list depending on what the user chooses*/}
+      {displayStyle === "grid" ? (
+        <div id="grid-container">
+          <JobCard jobDetails={jobDetails} />
+          <JobCard jobDetails={jobDetails} />
+          <JobCard jobDetails={jobDetails} />
+          <JobCard jobDetails={jobDetails} />
+        </div>
+      ) : (
+        <div>
+          <JobList jobDetails={jobDetails} />
+          <JobList jobDetails={jobDetails} />
+          <JobList jobDetails={jobDetails} />
+          <JobList jobDetails={jobDetails} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DisplayJobListings;
