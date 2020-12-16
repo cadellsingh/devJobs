98b7933027f3d5481330ae1b79c2b7ef9b58a696
Cadell Singh <cadellsingh9@gmail.com>
2020-12-15 22:58:07 -0400

    show more button added

---
+++ src/ShowMore.js
@@ -0,0 +1,9 @@
+const ShowMore = ({ handleOnClick }) => {
+  return (
+    <div className="show-more" onClick={handleOnClick}>
+      <p>Show More</p>
+    </div>
+  );
+};
+
+export default ShowMore;
