f97b7846a59aec6c4fe0e12eb5580b7e0d3fe75f
Cadell Singh <cadellsingh9@gmail.com>
2020-12-26 11:40:54 -0400

    Update NoResults.js
    component when no jobs are returned

--- src/NoResults.js
+++ src/NoResults.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 
+const StyledNoResults = styled.div`
+  text-align: center;
+  font-size: 20px;
+  margin-top: 30px;
+  color: ${({ theme }) => theme.text};
+  font-weight: 700;
+`;
+
 const NoResults = () => {
   return (
-    <div>
-      <p>0 jobs available</p>
-    </div>
+    <StyledNoResults>
+      <p>There aren't any jobs that match your search.</p>
+    </StyledNoResults>
   );
 };
 
